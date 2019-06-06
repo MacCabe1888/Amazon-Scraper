a8ac46fb4bc49c8399f6f7ff0ef4ae2c58d58b27
Cameron <cameronjmaccabe@gmail.com>
2019-06-05 17:26:15 -0700

    added bookTitle and bookAuthor to NoteSchema

--- models/Note.js
+++ models/Note.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const NoteSchema = new Schema({
+  bookTitle: String,
+  bookAuthor: String,
   title: String,
   body: String
 });
